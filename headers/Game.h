fb54a76e754edcc6882ca3196cc0b3477681baed
waffleprinter <dwamsie@gmail.com>
2024-10-13 01:40:07 -0400

    Initial commit

---
+++ headers/Game.h
@@ -0,0 +1,26 @@
+#ifndef CONWAY_REVISED_GAME_H
+#define CONWAY_REVISED_GAME_H
+
+#include <SFML/Graphics.hpp>
+#include <Board.h>
+
+class Game {
+private:
+    sf::RenderWindow window;
+    sf::Event event{};
+    Board board;
+
+    bool isPaused = false;
+
+    void pollEvents();
+    void update();
+    void draw();
+
+public:
+    explicit Game(Board &board);
+
+    void run();
+};
+
+
+#endif //CONWAY_REVISED_GAME_H
---
+++ src/Game.cpp
@@ -0,0 +1,38 @@
+#include "Game.h"
+
+Game::Game(Board &board) : board(board) {
+    this->window.create(sf::VideoMode(1080, 720), "Conway's Game of Life", sf::Style::Titlebar | sf::Style::Close);
+}
+
+void Game::pollEvents() {
+    while (this->window.pollEvent(this->event)) {
+        if (event.type == sf::Event::Closed)
+            window.close();
+
+        if (event.type == sf::Event::KeyPressed) {
+            if (event.key.code == sf::Keyboard::Escape)
+                window.close();
+        }
+    }
+}
+
+void Game::update() {
+    this->board.update();
+}
+
+void Game::draw() {
+    this->window.clear(sf::Color::White);
+
+    this->board.draw(window);
+
+    this->window.display();
+}
+
+void Game::run() {
+    while (this->window.isOpen()) {
+        this->pollEvents();
+        this->update();
+        this->draw();
+    }
+}
+
