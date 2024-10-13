6debaf3b812d9d7ca2ad1eed08a2275f30b155fe
waffleprinter <dwamsie@gmail.com>
2024-10-13 01:40:38 -0400

    Replaced code with implementation using Game

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,11 @@
-#include <iostream>
-
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
 #include "Board.h"
+#include "Game.h"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1080, 720), "Conway Revised", sf::Style::Titlebar | sf::Style::Close);
-    sf::Event event;
+    Board board = Board(10, 10, 10, sf::Vector2f(0, 0));
+    Game game = Game(board);
 
-    Board board = Board(10, 100, 100, sf::Vector2f(0, 0));
-    board.toggleCell(0, 1);
-    board.toggleCell(1, 2);
-    board.toggleCell(2, 0);
-    board.toggleCell(2, 1);
-    board.toggleCell(2, 2);
-
-    while (window.isOpen()) {
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-
-            if (event.type == sf::Event::KeyPressed)
-                if (event.key.code == sf::Keyboard::Escape)
-                    window.close();
-        }
-
-        board.update();
-
-        window.clear(sf::Color::White);
-
-        board.draw(window);
-
-        window.display();
-    }
+    game.run();
 
     return 0;
 }
