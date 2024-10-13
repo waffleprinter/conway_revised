ca76454b841376a4e748f294a45a53eca60a88ba
waffleprinter <dwamsie@gmail.com>
2024-10-13 13:19:51 -0400

    added glider test

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,15 @@
 #include "Game.h"
 
 int main() {
-    Board board = Board(10, 10, 10, sf::Vector2f(0, 0));
+    Board board = Board(10, 50, 50, sf::Vector2f(0, 0));
+
+    board.toggleCell(0, 1);
+    board.toggleCell(1, 2);
+    board.toggleCell(2, 0);
+    board.toggleCell(2, 1);
+    board.toggleCell(2, 2);
+
+
     Game game = Game(board);
 
     game.run();
