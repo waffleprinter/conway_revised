e0b65b807c9dd051a5ef280433d57f466eeffcf7
waffleprinter <dwamsie@gmail.com>
2024-10-13 20:02:17 -0400

    wanna get rid of changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,7 @@
 #include "Game.h"
 
 int main() {
-    Board board = Board(10, 50, 50, sf::Vector2f(0, 0));
-
-    board.toggleCell(0, 1);
-    board.toggleCell(1, 2);
-    board.toggleCell(2, 0);
-    board.toggleCell(2, 1);
-    board.toggleCell(2, 2);
-
-
+    Board board = Board(10, 50, 80, sf::Vector2f(100, 85));
     Game game = Game(board);
 
     game.run();
